fd7b12e7db3a5a86a9abd3b3dce7678859c4a16b
Mendoza Murillo Victor Daniel <mendozamv2018@dci25-185.ugto.mx>
2018-05-12 08:28:38 -0500

    Cambios

---
+++ semana1/Programa_hola.c
@@ -0,0 +1,19 @@
+
+/* Esto es un comentario. Será el primer programa para empezar el curso 
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por Victor Daniel Mendoza Murillo*/
+
+#include<stdio.h>
+
+int main()
+{
+
+
+printf("Hola Daniel\n");
+printf("Este es mi primer programa de este curso curso\n");
+
+}
+
+/*Terminé de escribir el programa*/
+
+
---
+++ semana1/primer_programa.c
@@ -0,0 +1,22 @@
+
+/* Esto es un comentario. Será el primer programa para empezar el curso 
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por Victor Daniel Mendoza Murillo*/
+
+#include<stdio.h>
+
+int main()
+{
+
+
+printf("Hola Daniel\n");
+printf("Este es mi primer programa de este curso curso\n");
+printf("Introduce tu edad:\n");
+scanf("%i",19);
+printf("\nTu edad es: %i \n");
+return 0;
+}
+
+/*Terminé de escribir el programa*/
+
+
